26425f17466c2fe66bf50ad8636e480bddd10267
Adrian Mandee <mandee.adrian@gmail.com>
2018-06-26 21:44:30 -0700

    Added activities

--- app/src/main/java/com/example/almandee/cryptoexchange/MainMenu.java
+++ app/src/main/java/com/example/almandee/cryptoexchange/MainMenu.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.coinbase.android.sdk.OAuth;
 import com.coinbase.api.Coinbase;
@@ -93,6 +95,19 @@ public class MainMenu extends Activity{
         } catch (CoinbaseException ex) {
             mTextView.setText("There was an error redirecting to Coinbase: " + ex.getMessage());
         }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        Button clickButton = findViewById(R.id.btnlogin);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startMain =new Intent(getApplicationContext(), MainMenu.class);
+                startActivity(startMain);
+
+            }
+        });
     }
 
     @Override
@@ -115,5 +130,17 @@ public class MainMenu extends Activity{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void goToSend(View view){
+        Intent goToSend =new Intent(getApplicationContext(), SendMenu.class);
+        startActivity(goToSend);
+    }
+
+
+    public void goToRequest(View view){
+        Intent goToRequest =new Intent(getApplicationContext(), RequestMenu.class);
+        startActivity(goToRequest);
     }
 }
---
+++ app/src/main/java/com/example/almandee/cryptoexchange/SendMenu.java
@@ -0,0 +1,21 @@
+package com.example.almandee.cryptoexchange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SendMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
+
+
+
